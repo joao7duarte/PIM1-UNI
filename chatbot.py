b581e5569d19151efeda044846beaee109adb795
Joao Gabriel <jgabrieldc@gmail.com>
2025-11-13 13:24:35 -0300

    update de modelo LLM e prompt personalizado

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,20 @@
 import subprocess
 
 def chat(prompt):
-    system_prompt = f"Responda sempre em português do Brasil de forma natural, clara e educada. {prompt}"
+    contexto = (
+        "Você é um assistente dentro de um Sistema Acadêmico Colaborativo com apoio de Inteligência Artificial. "
+        "O sistema foi desenvolvido em C, possui interface em Tkinter e integração com um chatbot. "
+        "Os usuários podem ser alunos ou professores, e o chatbot deve ajudar tirando dúvidas sobre o uso do sistema, "
+        "como login, cadastro, lançamento de notas e visualização de desempenho."
+    )
+
+    system_prompt = f"Responda sempre em português do Brasil de forma natural, clara e educada." f"{contexto}\nUsuário: {prompt}"
 
     result = subprocess.run(
-        ["C:\\Users\\jgabr\\AppData\\Local\\Programs\\Ollama\\ollama.exe", "run", "phi3", system_prompt],
+        ["C:\\Users\\jgabr\\AppData\\Local\\Programs\\Ollama\\ollama.exe", "run", "gemma3:1b", system_prompt],
         capture_output=True, text=True, encoding= 'utf-8'
     )
-    print("GPT-mini:", result.stdout.strip())
+    print("Ollama:", result.stdout.strip())
 
 while True:
     user = input("Você: ")
